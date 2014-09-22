69a56d02a7f748e021b021ca7df4b4de3c1eee3b
Niko Montonen <nikomo@nikomo.fi>
2014-09-22 13:53:40 -0600

    Continuous mode
    Code runs continuously, break out with empty input
    Print signal being sent to console

--- run.py
+++ run.py
@@ -3,9 +3,14 @@
 import control
 from morse import morse
 
-userinput = input("Enter a phrase: ")
-for character in userinput:
-    signals = morse[character.upper()]
-    for signal in signals:
-        control.blink(signal)
+while True:
+    userinput = input("Enter a phrase, or press enter to quit: ")
+    if userinput == "":
+        quit()
 
+    for character in userinput:
+        signals = morse[character.upper()]
+        for signal in signals:
+            print(signal, end="", flush=True)
+            control.blink(signal)
+    print()
